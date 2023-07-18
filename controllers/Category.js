6be8b70acfbf10d6d73a32f7ab2dec7d27ff64c0
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2023-07-19 00:56:57 +0500

    updates

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,13 +1,11 @@
 const getCategory = (req,res) => {
     res.status(200).json({message : `The Category is : `})
 }
-const createCategory = (req,res) => {
-    res.status(200).json({message : `Creating the Category`});
-}
+
 const updateCategory =(req,res) => {
     res.status(200).json({message : `Updating the category`});
 }
 const deleteCategory = (req,res) => {
     res.status(200).json({message : `Deleting the Category`});
 }
-module.exports = {getCategory , createCategory ,updateCategory , deleteCategory} ;
+module.exports = {getCategory ,updateCategory , deleteCategory} ;
--- routers/Category.js
+++ routers/Category.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const categoryRouter = express.Router();
 const {getCategory ,createCategory, updateCategory , deleteCategory}   = require("../controllers/Category");
- 
+const { getAll, createOne, updateOne, deleteOne } = require("../utils/api-utils");
+const Category = require("../models/Category");
 
-categoryRouter.route("/").post(createCategory);
-categoryRouter.route("/:id").get(getCategory).patch(updateCategory).delete(deleteCategory) ;
+
+categoryRouter.route("/").post(createOne(Category));
+categoryRouter.route("/:id").get(getCategory).patch(updateOne(Category)).delete(deleteOne(Category)) ;
+categoryRouter.route("/").get(getAll(Category));
 
 module.exports = categoryRouter;
